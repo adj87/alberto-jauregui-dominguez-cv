f3bc01699e67b11e4387702dbecd9f548da999cc
alberto jauregui dominguez <ajauregui@wtelecom.es>
2022-07-05 00:30:16 +0200

    make topbar sticky

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import { Initial, Education } from "./pages";
+import { TopBar } from "./pages/Education/Topbar";
 
 function App() {
   return (
     <div>
       <Initial />
+      <TopBar />
+      <Education />
       <Education />
     </div>
   );
--- src/pages/Education/index.js
+++ src/pages/Education/index.js
@@ -1,10 +1,5 @@
 import React from "react";
-import { TopBar } from "./Topbar";
 
 export const Education = () => {
-  return (
-    <div className="h-screen bg-gray-500">
-      <TopBar />
-    </div>
-  );
+  return <div className="h-screen bg-gray-500"></div>;
 };
