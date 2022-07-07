6bc172f0c16138721863572e085b9542e5b55707
alberto jauregui dominguez <ajauregui@wtelecom.es>
2022-07-07 20:08:35 +0200

    change langs component

--- src/components/Langs.js
+++ src/components/Langs.js
@@ -2,14 +2,14 @@ import React from "react";
 import { useState } from "react";
 
 const Langs = () => {
-  const [lang, setLang] = useState("Es");
-  const langs = ["Es", "En"];
+  const [lang, setLang] = useState("es");
+  const langs = ["es", "en"];
   return (
-    <div className="flex ml-3 text-2xl">
+    <div className="flex ml-3 text-xl">
       {langs.map((el) => (
         <span
           className={`md:ml-4 ml-3 cursor-pointer ${
-            el === lang ? "text-white bold" : "text-gray-600"
+            el === lang ? "text-gray-300" : "text-gray-600"
           }`}
           onClick={() => setLang(el)}
         >
