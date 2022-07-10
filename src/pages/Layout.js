be92b4f5834ee74d518fdc9398b8b649e6c45ded
alberto jauregui dominguez <ajauregui@wtelecom.es>
2022-07-10 17:29:23 +0200

    more progress

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useRef } from "react";
+import useNearScreen from "../hooks/useNearScreen";
 
-const Layout = ({ children }) => (
-  <div className="md:max-w-screen-md px-6 md:px-8 m-auto mt-12">{children}</div>
-);
+const Layout = ({ children }) => {
+  const externalRef = useRef();
+  const { isNearScreen } = useNearScreen({
+    externalRef,
+    distance: "10px",
+    once: false,
+    threshold: 0.4
+  });
+
+  const className = isNearScreen ? "opacity-1" : "opacity-20";
+  return (
+    <div
+      className={`md:max-w-screen-md px-6 md:px-8 m-auto mt-12 transition duration-1000  ${className}`}
+      ref={externalRef}
+    >
+      {children}
+    </div>
+  );
+};
 
 export default Layout;
