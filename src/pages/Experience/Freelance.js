738659f098590bd699b4cad5b086e89c338a3aaa
alberto jauregui dominguez <ajauregui@wtelecom.es>
2022-07-30 19:18:52 +0200

    fix some grammar errors

--- src/pages/Experience/Freelance.js
+++ src/pages/Experience/Freelance.js
@@ -2,10 +2,10 @@ import React from "react";
 import Layout from "./Layout";
 
 const paragraphs = [
-  "Development of FrontEnd application for the sales man of a company. The application allows the sales man to create,read,edit and delete orders,customers,fares as well as to export catalogs in PDF format",
-  "Development of a MVC backend architecture pattern in PHP from scratch",
+  "Development of FrontEnd application for company sales team. The application allows the sales team to create, read, edit and delete orders, customers and fares, and to export catalogs in PDF format.",
+  "Build MVC backend architecture pattern in PHP from scratch",
   "Development of plugins for wordpress using PHP and vanilla JS.",
-  "Development of a website application for football trainners and technical stuff. It enables you to create trainning footballs session  by selecting football trainning football exercises from a very large and extensive library of exercises"
+  "Development of a website application for football trainers and technical staff. It enables the creation of football training sessions with an extensive library of exercises"
 ];
 
 const techs = [
--- src/pages/Experience/Wellness.js
+++ src/pages/Experience/Wellness.js
@@ -5,8 +5,8 @@ const paragraphs = [
   "Development of Frontend applications using React, Typescript, Ant design, and Apollo-Client for managing application states.",
   "Use of Webpack Module Federation for creating a Microfrontends environment",
   "Development of a ApolloServer as a gateway using NodeJS, Express and typescript",
-  "Development of multiples microservers using NodeJS with express framework and mongoDB",
-  "Use of dockers to deploy every microservices, gateway and frontend application"
+  "Development of multiple microservervices using NodeJS with express framework and mongoDB",
+  "Use of dockers to deploy microservices, gateway and frontend applications"
 ];
 
 const techs = [
